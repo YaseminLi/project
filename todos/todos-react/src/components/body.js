14afa6457ed1f2a5fa43976a20b81b1f737ecfff
YaseminLi <yaseminli@163.com>
2019-06-03 11:43:44 +0800

    todos-react

--- todos/todos-react/src/components/body.js
+++ todos/todos-react/src/components/body.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import  TodoList from './todo_list';
 export default class Body extends React.Component {
     constructor() {
         super();
         this.state = {
             inputValue: '',
-            todos: []
+            selectedFilter: ''
         }
     }
-    inputChange(e) {
+    componentWillMount() {
+        const url = document.URL.split('/#/')[1];
+        const selectedFilter = url ? url : 'all';
+        let todos=JSON.parse(localStorage.getItem('todos'));
+        todos=todos?todos:[];
+        this.setState({ selectedFilter,todos })
+    }
+    inputChange(e){
         this.setState({ inputValue: e.target.value })
     }
     //增加todoItem
@@ -17,67 +22,103 @@ export default class Body extends React.Component {
         if (e.keyCode == 13) {
             const newTodo = { title: e.target.value, completed: false };
             const todos = this.state.todos;
-            todos.push(newTodo);
+            todos.push(newTodo)
             this.setState({ todos, inputValue: '' });
+            localStorage.setItem('todos',JSON.stringify(todos))
         };
     }
-    //todoItem是否完成的状态
+    //todoItemga完成的状态切换
     changeChecked(index) {
         const { todos } = this.state;
         todos[index].completed = !todos[index].completed;
         this.setState({ todos });
+        localStorage.setItem('todos',JSON.stringify(todos))
     }
     //删除todoItem
     destroy(index) {
         const { todos } = this.state;
         todos.splice(index, 1);
         this.setState({ todos });
+        localStorage.setItem('todos',JSON.stringify(todos))
     }
     //清除完成的
     clearCompleted() {
-        console.log('clear')
         let { todos } = this.state;
         todos = todos.filter(item => { if (item.completed == false) return true });
         this.setState({ todos })
+        localStorage.setItem('todos',JSON.stringify(todos))
+    }
+    //被选中的filter保存至state中
+    selectedFilter(a) {
+        this.setState({
+            selectedFilter: a
+        });
+    }
+    //全部勾选
+    toggleAll() {
+        const { todos } = this.state;
+        const toggle = todos.find(e => e.completed == false);
+        if (toggle) {
+            todos.map(item => item.completed = true);
+        } else {
+            todos.map(item => item.completed = false);
+        }
+        this.setState({ todos });
+        localStorage.setItem('todos',JSON.stringify(todos))
     }
     render() {
-        const allTodos = this.state.todos;
-        const activeTodos = allTodos.filter(item => { if (item.completed == false) return true });
-        const completedTodos = allTodos.filter(item => { if (item.completed == true) return true });
-        // const todoList = todos.length ? todos.map((item, index) => (
-        //     <div key={index} className='todoItem'>
-        //         <label htmlFor={index} className={item.completed ? 'Complete' : ''}></label>
-        //         <input type='checkbox' id={index} checked={item.completed} onChange={this.changeChecked.bind(this, index)} />
-        //         <div className={item.completed ? 'itemComplete' : 'item'}>{item.title}</div>
-        //         <button className='destroy' onClick={this.destroy.bind(this, index)}>x</button>
-        //     </div>
-        // )) : '';
+        let { todos, selectedFilter } = this.state;
+        const activeTodos = todos.filter(item => { if (item.completed == false) return true });
+        const completedTodos =todos.filter(item => { if (item.completed == true) return true });
+        let currentTodos = [];
+        if (selectedFilter == 'active') {
+            currentTodos = activeTodos;
+        } else if (selectedFilter == 'completed') {
+            currentTodos = completedTodos;
+        } else {
+            currentTodos = todos;
+        }
+        const todoList = currentTodos? currentTodos.map((item, index) => (
+            <div key={index} className='todoItem'>
+                <label htmlFor={index} className={item.completed ? 'Complete' : ''}></label>
+                <input type='checkbox' id={index} checked={item.completed} onChange={this.changeChecked.bind(this, index)} />
+                <div className={item.completed ? 'itemComplete' : 'item'}>{item.title}</div>
+                <button className='destroy' onClick={this.destroy.bind(this, index)}>x</button>
+            </div>
+        )) : '';
         return (
             <div className='main'>
-                <input className='new-todo' placeholder='What needs to be done?' onKeyUp={e => this.onAddTodo(e)} value={this.state.inputValue} onChange={(e) => this.inputChange(e)} />
+                <div className='toggle'
+                    style={{ "display": todos.length!==0? "flex" : "none" }}
+                    onClick={this.toggleAll.bind(this)}
+                >
+                    <p className={todos.length == completedTodos.length ? 'toggle-all' : ''}>></p></div>
+                <input
+                    className='new-todo'
+                    placeholder='What needs to be done?'
+                    onKeyUp={e => this.onAddTodo(e)}
+                    value={this.state.inputValue}
+                    type="text"
+                    onChange={(e) => this.inputChange(e)}
+                />
                 <div className='todoList'>
-                    <Router>
-                        <Switch>
-                            <Route exact path='/' ><TodoList todos={allTodos}/></Route>
-                            <Route path='/active' ><TodoList todos={activeTodos}/></Route>
-                            <Route path='/completed' ><TodoList todos={completedTodos}/></Route>
-                        </Switch>
-                    </Router></div>
+                    {todoList}
+                </div>
 
-                <div className='footer' style={{ "display": allTodos.length !== 0 ? "flex" : "none" }} >
+                <div className='footer' style={{ "display": todos.length!==0? "flex" : "none" }} >
                     <div className='total-count'>
                         <span className='num'>{activeTodos.length} </span>
                         <span className='num-item'>{activeTodos.length < 2 ? 'item' : 'items'} </span>
                         <span>left</span>
                     </div>
                     <div className='filters'>
-                        <Router>
-                            <Link to="/"><span className='a'>All</span></Link>
-                            <Link to="/active"><span>Active</span></Link>
-                            <Link to="/complrete"><span>Complete</span></Link>
-                        </Router>
+                        <span onClick={this.selectedFilter.bind(this, 'all')} className={selectedFilter == 'all' ? 'selected' : 'no-selected'}>All</span>
+                        <span onClick={this.selectedFilter.bind(this, 'active')} className={selectedFilter == 'active' ? 'selected' : 'no-selected'}>Active</span>
+                        <span onClick={this.selectedFilter.bind(this, 'completed')} className={selectedFilter == 'completed' ? 'selected' : 'no-selected'}>Complete</span>
                     </div>
-                    <div onClick={this.clearCompleted.bind(this)}>clear completed</div>
+                    <div className='clear' onClick={this.clearCompleted.bind(this)}>
+                        <p style={{ "display": completedTodos.length !== 0 ? "flex" : "none" }}>clear completed</p>
+                    </div>
                 </div>
             </div>
         )
--- todos/todos-react/src/components/todo_list_router.js
+++ todos/todos-react/src/components/todo_list_router.js
@@ -9,9 +9,9 @@ export default class TodoList extends React.Component{
         const todoList = todos.length ? todos.map((item, index) => (
             <div key={index} className='todoItem'>
                 <label htmlFor={index} className={item.completed ? 'Complete' : ''}></label>
-                <input type='checkbox' id={index} checked={item.completed} onChange={this.changeChecked.bind(this, index)} />
+                <input type='checkbox' id={index} checked={item.completed} onChange={this.props.changeChecked} />
                 <div className={item.completed ? 'itemComplete' : 'item'}>{item.title}</div>
-                <button className='destroy' onClick={this.destroy.bind(this, index)}>x</button>
+                <button className='destroy' onClick={this.props.destroy}>x</button>
             </div>
         )) : '';
         return(
