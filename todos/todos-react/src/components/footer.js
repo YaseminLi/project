286f5e9a034146d82a765e83050c3ef6f7dc1221
YaseminLi <yaseminli@163.com>
2019-06-04 15:01:54 +0800

    edit 非受控组件

--- todos/todos-react/src/components/footer.js
+++ todos/todos-react/src/components/footer.js
@@ -1,12 +1,28 @@
 import React from 'react';
 export default class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+        const { totalCount, activeCount, filter } = this.props;
+        const clearButton = totalCount > activeCount ?
+            <div className='clear' onClick={this.props.clearCompleted}>
+                <p>clear completed</p>
+            </div> : '';
         return (
-            <footer >
-                <p>Double-click to edit a todo</p>
-                <p>Created by <a href="https://github.com/YaseminLi">Yasemin Li</a></p>
-                <p>Part of <a href="https://todomvc.com">TodoMVC</a></p>
-            </footer>
+            <div className='footer' >
+                <div className='total-count'>
+                    <span className='num'>{activeCount} </span>
+                    <span className='num-item'>{activeCount < 2 ? 'item' : 'items'} </span>
+                    <span>left</span>
+                </div>
+                <div className='filters'>
+                    <span id='all' onClick={this.props.selectedFilter} className={filter == 'all' ? 'selected' : 'no-selected'}>All</span>
+                    <span id='active' onClick={this.props.selectedFilter} className={filter == 'active' ? 'selected' : 'no-selected'}>Active</span>
+                    <span id='completed' onClick={this.props.selectedFilter} className={filter == 'completed' ? 'selected' : 'no-selected'}>Complete</span>
+                </div>
+                {clearButton}
+            </div>
         )
     }
 }
--- todos/todos-react/src/main.js
+++ todos/todos-react/src/main.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/header';
 import Body from './components/body';
-import Footer from './components/footer';
 class Root extends React.Component {
     render() {
         return (
             <div>
-                <Header />
+                <header >
+                    todos
+                </header>
                 <Body />
-                <Footer />
+                <footer >
+                    <p>Double-click to edit a todo</p>
+                    <p>Created by <a href="https://github.com/YaseminLi">Yasemin Li</a></p>
+                    <p>Part of <a href="https://todomvc.com">TodoMVC</a></p>
+                </footer>
             </div>
         )
     }
