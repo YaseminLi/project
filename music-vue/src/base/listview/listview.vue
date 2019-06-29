899434bcc1726c29642653706a8cbc66d757e13c
YaseminLi <yaseminli@163.com>
2019-06-29 20:14:58 +0800

    listview

---
+++ music-vue/src/base/listview/listview.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="listview">
+    <scroll class="singer-wrapper" :data="data" ref="singerWrapper">
+      <div>
+        <div class="singer-container" v-for="(item,index) in data" :key="index">
+          <h1 class="title">{{item.title}}</h1>
+          <div>
+            <div class="singer-item" v-for="(singer,index) in item.items" :key="index">
+              <img class="avatar" :src="singer.avatar">
+              <span class="name">{{singer.name}}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </scroll>
+    <div class="list-shortcut">
+      <span
+        @click="selectTitle(index)"
+        class="item"
+        v-for="(item,index) in data"
+        :key="index"
+      >{{index==0?'热':item.title}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import scroll from "base/scroll/scroll.vue";
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: null
+    }
+  },
+  components: {
+      scroll
+  }
+};
+</script>
+
+<style lang='stylus' >
+@import '~common/stylus/variable'
+.singer-wrapper
+  position: fixed
+  top: 44px
+  bottom: 0
+  width: 100%
+  overflow: hidden
+  .singer-container
+    padding-bottom: 30px
+    .title
+      height: 30px
+      line-height: 30px
+      padding-left: 20px
+      background: $color-background-dd
+      color: $color-text
+      font-size: 12px
+    .singer-item
+      width: 100%
+      box-sizing: border-box
+      padding: 20px 0 0 30px
+      display: flex
+      align-items: center
+      .avatar
+        border-radius: 50%
+        width: 50px
+        height: 50px
+      .name
+        font-size: 14px
+        color: $color-text
+        margin-left: 20px
+.list-shortcut
+  position: absolute
+  right: 0
+  bottom: 50px
+  width: 20px
+  padding: 20px 0
+  border-radius: 10px
+  background: $color-background-dd
+  display: flex
+  flex-direction: column
+  .item
+    font-size: 12px
+    height: 12px
+    width: 14px
+    color: $color-text
+    padding: 3px
+    background: $color-background-dd
+    color: $color-text
+    text-align: center
+</style>
--- music-vue/src/components/singer/singer.vue
+++ music-vue/src/components/singer/singer.vue
@@ -1,34 +1,14 @@
 <template>
   <div class="singer">
-    <scroll class="singer-wrapper" :data="singer" ref="singerWrapper">
-      <div>
-        <div class="singer-container" v-for="(item,index) in singer" :key="index">
-          <h1 class="title">{{item.title}}</h1>
-          <div>
-            <div class="singer-item" v-for="(singer,index) in item.items" :key="index">
-              <img class="avatar" :src="singer.avatar">
-              <span class="name">{{singer.name}}</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </scroll>
-    <div class="list-shortcut">
-      <span
-        @click="selectTitle(index)"
-        class="item"
-        v-for="(item,index) in singer"
-        :key="index"
-      >{{index==0?'热':item.title}}</span>
-    </div>
+   <listview :data='singers'/>
   </div>
 </template>
 
 <script>
 import { getSinger } from "api/singer.js";
 import { ERR_OK } from "api/config.js";
-import scroll from "base/scroll/scroll.vue";
 import Singer from "common/js/singer.js";
+import listview from 'base/listview/listview';
 
 const HOT_SINGER_LEN = 10;
 const HOT_NAME = "热门";
@@ -36,7 +16,7 @@ const HOT_NAME = "热门";
 export default {
   data() {
     return {
-      singer: []
+      singers: []
     };
   },
   created() {
@@ -53,7 +33,7 @@ export default {
     _getSinger() {
       getSinger().then(res => {
         if (res.code === ERR_OK) {
-          this.singer = this._normalizeSinger(res.data.list);
+          this.singers = this._normalizeSinger(res.data.list);
         }
       });
     },
@@ -100,60 +80,11 @@ export default {
     }
   },
   components: {
-    scroll
+    listview
   }
 };
 </script>
 
 <style scoped lang="stylus">
-@import '~common/stylus/variable'
-@import '~common/stylus/mixin'
-.singer-wrapper
-  position: fixed
-  top: 44px
-  bottom: 0
-  width: 100%
-  overflow: hidden
-  .singer-container
-    padding-bottom: 30px
-    .title
-      height: 30px
-      line-height: 30px
-      padding-left: 20px
-      background: $color-background-dd
-      color: $color-text
-      font-size: 12px
-    .singer-item
-      width: 100%
-      box-sizing: border-box
-      padding: 20px 0 0 30px
-      display: flex
-      align-items: center
-      .avatar
-        border-radius: 50%
-        width: 50px
-        height: 50px
-      .name
-        font-size: 14px
-        color: $color-text
-        margin-left: 20px
-.list-shortcut
-  position: absolute
-  right: 0
-  bottom: 50px
-  width: 20px
-  padding: 20px 0
-  border-radius: 10px
-  background: $color-background-dd
-  display: flex
-  flex-direction: column
-  .item
-    font-size: 12px
-    height: 12px
-    width: 14px
-    color: $color-text
-    padding: 3px
-    background: $color-background-dd
-    color: $color-text
-    text-align: center
+
 </style>
