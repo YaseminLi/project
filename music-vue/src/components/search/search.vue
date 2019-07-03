56d6d8a6b24c7370b39c1243ee3fdee4c60bffac
YaseminLi <yaseminli@163.com>
2019-07-03 18:11:24 +0800

    热搜关键词获取

--- music-vue/src/components/search/search.vue
+++ music-vue/src/components/search/search.vue
@@ -5,12 +5,30 @@
 </template>
 
 <script>
+import {getSearch} from 'api/search.js';
+import {ERR_OK} from 'api/config.js';
     export default {
-        
+        data(){
+            return {
+                hotkey:[]
+            }
+        },
+        created(){
+            this._initSearch();
+        },
+        methods:{
+            _initSearch(){
+                getSearch().then(res=>{
+                    if(res.code==ERR_OK)
+                    console.log(res.data);
+                    this.hotkey=res.data;
+                })
+            }
+        }
     }
 </script>
 
-<style scoped lang="stylus">
+<style lang="stylus">
 
 
 </style>
